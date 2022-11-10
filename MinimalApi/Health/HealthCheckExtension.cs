c74b20b0b36d416441d8b493f58263e610adc931
mekkl <mikkelemil1501@gmail.com>
2022-11-10 21:18:15 +0100

    Add health checks

---
+++ MinimalApi/Health/HealthCheckExtension.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Text.Json;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MinimalApi.Health;
+
+public static class HealthCheckExtension
+{
+    public static void AddAppHealthChecks(this IServiceCollection services)
+    {
+        services.AddHealthChecks()
+            .AddCheck<LiveCheck>("live")
+            .AddCheck<ReadyCheck>("ready");
+    }
+    
+    public static void UseHealthChecks(this WebApplication app)
+    {
+        app.MapHealthChecks("/health/health", new HealthCheckOptions
+        {
+            ResponseWriter = async (context, report) =>
+            {
+                context.Response.ContentType = "application/json";
+
+                var response = new HealthCheckResponse
+                {
+                    HealthStatus = report.Status,
+                    HealthChecks = report.Entries.Select(check => new HealthCheck
+                    {
+                        Component = check.Key,
+                        HealthStatus = check.Value.Status,
+                        Description = check.Value.Description ?? string.Empty,
+                        Duration = check.Value.Duration,
+                        Exception = check.Value.Exception
+                    }),
+                    Duration = report.TotalDuration,
+            
+                };
+
+                context.Response.StatusCode = response.HealthStatus == HealthStatus.Healthy 
+                    ? (int)HttpStatusCode.OK 
+                    : (int)HttpStatusCode.InternalServerError;
+
+                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+            }
+        });
+    }
+}
---
+++ MinimalApi/Health/ReadyCheck.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.Diagnostics.HealthChecks;
+
+namespace MinimalApi.Health;
+
+public class ReadyCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new())
+    {
+        return Task.FromResult(HealthCheckResult.Healthy("Ready"));
+    }
+}
