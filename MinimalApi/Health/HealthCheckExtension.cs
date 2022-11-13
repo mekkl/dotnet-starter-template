7110393dead48e2b4b2b68fa024b2dace7466d4b
mekkl <mikkelemil1501@gmail.com>
2022-11-13 16:40:37 +0100

    Initial swagger commit

--- MinimalApi/Health/HealthCheckExtension.cs
+++ MinimalApi/Health/HealthCheckExtension.cs
@@ -1,5 +1,4 @@
 ﻿using System.Net;
-using System.Text;
 using System.Text.Json;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
@@ -14,34 +13,39 @@ public static class HealthCheckExtension
             .AddCheck<LiveCheck>("live")
             .AddCheck<DbHealthCheck>("DbHealth");
     }
-    
+
     public static void UseHealthChecks(this WebApplication app)
     {
         app.MapHealthChecks("/health", new HealthCheckOptions
-        {
-            ResponseWriter = async (context, report) =>
             {
-                context.Response.ContentType = "application/json";
-
-                var response = new HealthCheckResponse
+                ResponseWriter = async (context, report) =>
                 {
-                    HealthStatus = report.Status,
-                    HealthChecks = report.Entries.Select(check => new HealthCheck
-                    {
-                        Component = check.Key,
-                        HealthStatus = check.Value.Status,
-                        Description = check.Value.Description ?? string.Empty,
-                        Duration = check.Value.Duration,
-                    }),
-                    Duration = report.TotalDuration,
-                };
+                    context.Response.ContentType = "application/json";
 
-                context.Response.StatusCode = response.HealthStatus == HealthStatus.Healthy 
-                    ? (int)HttpStatusCode.OK 
-                    : (int)HttpStatusCode.InternalServerError;
-                
-                await context.Response.WriteAsync(JsonSerializer.Serialize(response));
-            }
-        });
+                    var response = new HealthCheckResponse
+                    {
+                        HealthStatus = report.Status,
+                        HealthChecks = report.Entries.Select(check => new HealthCheck
+                        {
+                            Component = check.Key,
+                            HealthStatus = check.Value.Status,
+                            Description = check.Value.Description ?? string.Empty,
+                            Duration = check.Value.Duration,
+                        }),
+                        Duration = report.TotalDuration,
+                    };
+
+                    context.Response.StatusCode = response.HealthStatus == HealthStatus.Healthy
+                        ? (int)HttpStatusCode.OK
+                        : (int)HttpStatusCode.ServiceUnavailable;
+
+                    await context.Response.WriteAsync(JsonSerializer.Serialize(response));
+                }
+            })
+            .WithName("Health")
+            .WithDisplayName("Health")
+            .WithSummary("Get services health status")
+            .WithDescription("Checks the utilized services health status")
+            .WithOpenApi();
     }
 }
--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -1,13 +1,35 @@
 using Application;
 using Infrastructure;
 using Infrastructure.Persistence;
+using Microsoft.OpenApi.Models;
 using MinimalApi.Health;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Version = "v1",
+        Title = ".NET 7 swagger",
+        Description = ".NET 7 starter template swagger doc",
+        TermsOfService = new Uri("https://github.com/mekkl/dotnet-starter-template"),
+        Contact = new OpenApiContact
+        {
+            Name = "Mekkl",
+            Url = new Uri("https://github.com/mekkl")
+        },
+        License = new OpenApiLicense
+        {
+            Name = "MIT License",
+            Url = new Uri("https://github.com/mekkl/dotnet-starter-template/blob/main/LICENSE")
+        }
+    });
+});
+
 builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.AddApplication();
-
 builder.Services.AddAppHealthChecks();
 
 var app = builder.Build();
@@ -20,7 +42,7 @@ if (app.Environment.IsDevelopment())
 
     // Initialise and seed database
     using var scope = app.Services.CreateScope();
-    
+
     var initializer = scope.ServiceProvider.GetRequiredService<AppDbContextInitializer>();
     await initializer.InitialiseAsync();
     await initializer.SeedAsync();
@@ -29,18 +51,22 @@ else
 {
     // Initialise database
     using var scope = app.Services.CreateScope();
-    
+
     var initializer = scope.ServiceProvider.GetRequiredService<AppDbContextInitializer>();
     await initializer.InitialiseAsync();
-    
+
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
+app.UseSwagger();
+app.UseSwaggerUI(options =>
+{
+    options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+    options.RoutePrefix = string.Empty;
+});
+
 app.UseHealthChecks();
-
 app.UseHttpsRedirection();
-
-app.MapGet("/", () => "Hello World!");
 
 app.Run();
