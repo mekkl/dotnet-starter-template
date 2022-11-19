2fdb9b165d9e036955d52fd03abd19fe2f007712
mekkl <26537575+mekkl@users.noreply.github.com>
2022-11-19 19:23:44 +0100

    Add initial hosted service (#28)
    * Add initial hosted service
    
    * Ensure background work

---
+++ MinimalApi/Extensions/SwaggerExtension.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace MinimalApi.Extensions;
+
+public static class SwaggerExtension
+{
+    public static void AddSwagger(this IServiceCollection services)
+    {
+        services.AddEndpointsApiExplorer();
+        services.AddSwaggerGen(options =>
+        {
+            options.SwaggerDoc("v1", new OpenApiInfo
+            {
+                Version = "v1",
+                Title = ".NET 7 swagger",
+                Description = ".NET 7 starter template swagger doc",
+                TermsOfService = new Uri("https://github.com/mekkl/dotnet-starter-template"),
+                Contact = new OpenApiContact
+                {
+                    Name = "Mekkl",
+                    Url = new Uri("https://github.com/mekkl")
+                },
+                License = new OpenApiLicense
+                {
+                    Name = "MIT License",
+                    Url = new Uri("https://github.com/mekkl/dotnet-starter-template/blob/main/LICENSE")
+                }
+            });
+        });
+    }
+
+    public static void UseSwaggerSetup(this WebApplication? app)
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI(options =>
+        {
+            options.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+            options.RoutePrefix = string.Empty;
+        });
+    }
+}
---
+++ MinimalApi/HostedServices/TimedHostedService.cs
@@ -0,0 +1,34 @@
+﻿using Shared.Attributes;
+using Shared.Common;
+
+namespace MinimalApi.HostedServices;
+
+[IgnoreCoverage]
+public class TimedHostedService : IHostedService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    public TimedHostedService(IServiceProvider serviceProvider)
+        => _serviceProvider = serviceProvider;
+
+    public Task StartAsync(CancellationToken cancellationToken)
+    {
+        Task.Run(() => DoWork(cancellationToken), cancellationToken);
+        return Task.CompletedTask;
+    }
+
+    private async Task DoWork(CancellationToken cancellationToken)
+    {
+        using var scope = _serviceProvider.CreateScope();
+        var logger = _serviceProvider.GetRequiredService<ILogger<TimedHostedService>>();
+        const int seconds = 60;
+        
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            logger.LogInformation("Hello world every {Seconds} seconds!", seconds);
+            await Delay.Seconds(seconds);
+        }
+    }
+
+    public Task StopAsync(CancellationToken cancellationToken) => Task.CompletedTask;
+}
