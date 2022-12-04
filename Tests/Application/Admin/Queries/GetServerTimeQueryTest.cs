97e29082c2b0cc46a54e441a39b4c23960add944
mekkl <mikkelemil1501@gmail.com>
2022-12-04 11:38:33 +0100

    Add query test

---
+++ Tests/Application/Admin/Queries/GetServerTimeQueryTest.cs
@@ -0,0 +1,18 @@
+﻿using Application.Admin.Queries;
+using FluentAssertions;
+
+namespace Tests.Application.Admin.Queries;
+
+public class GetServerTimeQueryTest
+{
+    [Fact]
+     public async Task Test()
+     {
+         var query = new GetServerTimeQueryHandler();
+         var input = new GetServerTimeQuery();
+
+         var actual = await query.Handle(input, CancellationToken.None);
+
+         actual.Should().BeCloseTo(DateTimeOffset.Now, TimeSpan.FromSeconds(2));
+     }
+}
