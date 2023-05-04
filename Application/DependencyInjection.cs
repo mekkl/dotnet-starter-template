65a58e0f98176c9ee4356464b57fd337fd88ebe9
mekkl <mikkelemil1501@gmail.com>
2023-05-04 19:18:52 +0200

    Upgrade versions

--- Application/DependencyInjection.cs
+++ Application/DependencyInjection.cs
@@ -1,6 +1,5 @@
 ﻿using System.Reflection;
 using FluentValidation;
-using MediatR;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Application;
@@ -10,7 +9,7 @@ public static class DependencyInjection
     public static IServiceCollection AddApplication(this IServiceCollection services)
     {
         services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
-        services.AddMediatR(Assembly.GetExecutingAssembly());
+        services.AddMediatR(cfg => cfg.RegisterServicesFromAssemblies(Assembly.GetExecutingAssembly()));
         
         return services;
     }
